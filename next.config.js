38d6355ebd7522c60781a02c332e560e72c08521
matthewrapp <mattrapp25@gmail.com>
2022-09-20 13:08:10 -0600

    one more push

---
+++ next.config.js
@@ -0,0 +1,29 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // reactStrictMode: false,
+    // swcMinify: true,
+    // eslint: {
+    //   ignoreDuringBuilds: true
+    // },
+    // async rewrites() {},
+    // async redirects() {
+    //   return [
+    //     {
+    //       source: '/listen',
+    //       destination: 'https://listen.mvttmusic.com/platforms',
+    //       permanent: false,
+    //     },
+    //   ]
+    // },
+    // webpack(config) {
+    //   config.module.rules.push({
+    //     test: /\.svg$/,
+    //     use: ["@svgr/webpack"]
+    //   });
+  
+    //   return config;
+    // }
+  }
+  
+  module.exports = nextConfig
+  
